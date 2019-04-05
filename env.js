3853b64ceecbdbaf9b88c4071acca629baca75e0
BeeJay <beejay.billante@gmail.com>
2019-04-05 14:18:57 -0500

    fix heroku

--- env.js
+++ env.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 module.exports = {
     devServer: {
       host: 'localhost',
       port: 5000
     }
   };
-=======
-module.exports = {
-    devServer: {
-      host: 'localhost',
-      port: 5000
-    }
-  };
->>>>>>> 5bec8e0745cd7161fa4b3b39e8a5e26ff3325e01
   
